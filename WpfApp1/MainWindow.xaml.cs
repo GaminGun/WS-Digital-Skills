cb266e8d2d72e60336118e209b365f1b6e8d3153
ET GaminGun <GaminGun@mail.ru>
2023-07-05 12:50:19 +0300

    Load application Session One

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (Application.Current.Windows.Count > 2)
+            {
+                MessageBox.Show("Завершите сессию.");
+            }
+            else
+            {
+                using (var db = new Session1_22Entities())
+                {
+                    var user = db.Users.AsNoTracking().FirstOrDefault(u => u.Email == LoginInput.Text && u.Password == PasswordInput.Password);
+                    var login = db.Users.AsNoTracking().FirstOrDefault(u => u.Email == LoginInput.Text);
+                    if (login == null)
+                    {
+                        ErrorAutho.Text = "Пользователь не найден.";
+                        errors.AppendLine("Пользователь не найден.");
+                    }
+                    else if (user == null)
+                    {
+                        ErrorAutho.Text = "Неверный пароль.";
+                        errors.AppendLine("Неверный пароль.");
+                    }
+                    else if (user.Active == false)
+                    {
+                        ErrorAutho.Text = "Вы заблокированы.";
+                        errors.AppendLine("Вы заблокированы.");
+                    }
+                    if (errors.Length == 0)
+                    {
+                        if (user.RoleID == 1)
+                        {
+                            AccessWin adminWin = new AccessWin();
+                            adminWin.Show();
+                        }
+                        else if (user.RoleID == 2)
+                        {
+                            UserWin userWin = new UserWin(user);
+                            userWin.Show();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void PassEmail_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            char inp = e.Text[0];
+            if ((inp < 'a' || inp > 'z') && (inp < 'A' || inp > 'Z') && (inp < '0' || inp > '9') && (inp < '!' || inp > '@'))
+                e.Handled = true;
+        }
+    }
+}
---
+++ WpfApp1/UserWin.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWin.xaml
+    /// </summary>
+    public partial class UserWin : Window
+    {
+        public Users trackUser;
+        public ActivityUsers tracker = new ActivityUsers();
+        public UserWin(Users info)
+        {
+            InitializeComponent();
+            Welcome.Text = $"Hi {info.FirstName}, Welcome to AMONIC Airlines.";
+            trackUser = info;
+            using (var db = new Session1_22Entities())
+            {
+                var error = db.ActivityUsers.AsNoTracking().FirstOrDefault(p => info.ID == p.IDUser && p.LogoutTime == null);
+                if (error != null)
+                {
+                    db.Entry(error).State = System.Data.Entity.EntityState.Deleted;
+                    db.SaveChanges();
+                    Tracking track = new Tracking(infoActive: error);
+                    track.ShowDialog();
+                }
+                var user = db.ActivityUsers.AsNoTracking().FirstOrDefault(p => info.ID == p.IDUser);
+                var timeNoNull = db.ActivityUsers.AsNoTracking().FirstOrDefault(p => info.ID == p.IDUser && p.AllTimeSpent != TimeSpan.Zero);
+                if (user == null || user.AllTimeSpent == TimeSpan.Zero && timeNoNull == null)
+                {
+                    tracker.AllTimeSpent = TimeSpan.Zero;
+                }
+                else
+                {
+                    UserInfo.ItemsSource = Session1_22Entities.GetContext().ActivityUsers.ToList().FindAll(p => info.ID == p.IDUser);
+                    tracker.AllTimeSpent = timeNoNull.AllTimeSpent;
+                }
+            }
+            CrashNumber();
+            tracker.IDUser = trackUser.ID;
+            tracker.Date = DateTime.Now;
+            TimerStart();
+            tracker.LoginTime = TimeSpan.Parse(DateTime.Now.ToLongTimeString());
+            Session1_22Entities.GetContext().ActivityUsers.Add(tracker);
+            try
+            {
+                Session1_22Entities.GetContext().SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            tracker.LogoutTime = TimeSpan.Parse(DateTime.Now.ToLongTimeString());
+            tracker.TimeSpent = tracker.LogoutTime - tracker.LoginTime;
+            try
+            {
+                tracker.AllTimeSpent += tracker.TimeSpent;
+                Session1_22Entities.GetContext().SaveChanges();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                tracker.AllTimeSpent += TimeSpan.Zero;
+                MessageBox.Show(ex.Message.ToString());
+                this.Close();
+            }
+            this.Close();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Session1_22Entities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                UserInfo.ItemsSource = Session1_22Entities.GetContext().ActivityUsers.ToList().FindAll(p => trackUser.ID == p.IDUser);
+            }
+        }
+        DispatcherTimer timer = null;
+        int hour = 0, min = 0, sec = 0;
+        public void TimerStart()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(TimerTick);
+            timer.Start();
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec == 60)
+            {
+                sec = 0;
+                min++;
+            }
+            if (min == 60)
+            {
+                min = 0;
+                hour++;
+            }
+            TimeOutput(hour, min, sec);
+        }
+        void TimeOutput(int hour, int min, int sec)
+        {
+            string _hour, _min, _sec;
+            if (sec < 10)
+                _sec = $"0{sec}";
+            else
+                _sec = $"{sec}";
+
+            if (min < 10)
+                _min = $"0{min}";
+            else
+                _min = $"{min}";
+
+            if (hour < 10)
+                _hour = $"0{hour}";
+            else
+                _hour = $"{hour}";
+            Time.Text = $"{_hour}:{_min}:{_sec}";
+        }
+        void CrashNumber()
+        {
+            List<ActivityUsers> users = Session1_22Entities.GetContext().ActivityUsers.ToList().FindAll(p => p.TypeError != null);
+            NumberCrashing.Text = users.Count().ToString();
+        }
+    }
+}
